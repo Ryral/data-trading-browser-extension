03df7d934b97aa7f6c125cc5be9f4722d1584e5c
Ryral <artnerdyo@gmail.com>
2025-10-12 15:01:08 +1300

    Browser extension styling

---
+++ popup.js
@@ -0,0 +1,7 @@
+// This function runs when the popup is clicked
+document.addEventListener("DOMContentLoaded", () => {
+    // Generates lucide icons - makes whatever in lucide.js usable
+    if (typeof lucide !== "undefined") {
+        lucide.createIcons();
+    }
+});
