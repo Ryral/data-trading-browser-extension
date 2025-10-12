0f53e27fd18e67518e06a7a556e29ade77f32465
Ryral <artnerdyo@gmail.com>
2025-10-12 15:20:12 +1300

    Created pages templates + style adjusting
    Made navigation for buttons

--- popup.js
+++ popup.js
@@ -4,4 +4,9 @@ document.addEventListener("DOMContentLoaded", () => {
     if (typeof lucide !== "undefined") {
         lucide.createIcons();
     }
+
+    document.getElementById("marketplace").addEventListener("click", () => chrome.tabs.create({url: "marketplace.html"}));
+    document.getElementById("dashboard").addEventListener("click", () => chrome.tabs.create({url: "dashboard.html"}));
+    document.getElementById("browserhistory").addEventListener("click", () => chrome.tabs.create({url: "browserhistory.html"}));
+    document.getElementById("settings").addEventListener("click", () => chrome.tabs.create({url: "settings.html"}));
 });
